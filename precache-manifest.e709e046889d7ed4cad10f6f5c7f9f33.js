e2155202f79378ee7fd9a540ce45ee631acaee1c
ashwin <ashwinvemula9@gmail.com>
2020-01-04 18:17:55 +0530

    Updates

---
+++ precache-manifest.e709e046889d7ed4cad10f6f5c7f9f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9711be26e855d082789b5de381d93a",
+    "url": "/vedantuTask/index.html"
+  },
+  {
+    "revision": "562f33adbd28820395d3",
+    "url": "/vedantuTask/static/css/main.63bac6d9.chunk.css"
+  },
+  {
+    "revision": "1338c3f3eafee4b9ff8d",
+    "url": "/vedantuTask/static/js/2.228fe6da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/vedantuTask/static/js/2.228fe6da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "562f33adbd28820395d3",
+    "url": "/vedantuTask/static/js/main.52e3c026.chunk.js"
+  },
+  {
+    "revision": "5fec38aa255a304f4d84",
+    "url": "/vedantuTask/static/js/runtime-main.469f0419.js"
+  }
+]);
